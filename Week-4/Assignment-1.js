1d2d9f42ef7acb92c430d600a546ce97b091a8ed
5a6r1na <119278456+5a6r1na@users.noreply.github.com>
2023-02-03 21:55:10 +0800

    modified files

--- Week-4/Assignment-1.js
+++ Week-4/Assignment-1.js
@@ -1,34 +1,32 @@
 //A
 function delayedResult(n1, n2, delayTime, callback) {
-    setTimeout(()=>{
-        let result = `${n1+n2} (${n1}+${n2})`;
-        callback(result);
-    }, delayTime);
-   }
+  setTimeout(() => {
+    let result = `${n1 + n2} (${n1}+${n2})`;
+    callback(result);
+  }, delayTime);
+}
 delayedResult(4, 5, 3000, function (result) {
-    console.log(result);
-   }); // 9 (4+5) will be shown in the console after 3 seconds
+  console.log(result);
+}); // 9 (4+5) will be shown in the console after 3 seconds
 delayedResult(-5, 10, 2000, function (result) {
-    console.log(result);
-   }); // 5 (-5+10) will be shown in the console after 2 seconds
-
+  console.log(result);
+}); // 5 (-5+10) will be shown in the console after 2 seconds
 
 //B
 function delayedResultPromise(n1, n2, delayTime) {
-    return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-           resolve(`${n1+n2} (${n1}+${n2})`);
-        }, delayTime);
-    });
-   }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`${n1 + n2} (${n1}+${n2})`);
+    }, delayTime);
+  });
+}
 delayedResultPromise(4, 5, 3000).then(console.log);
 // 9 (4+5) will be shown in the console after 3 seconds
 
-
 //C
 async function main() {
-    let results = await delayedResultPromise(4, 5, 3000);
-    console.log(results);
-    // your code here, you should call delayedResultPromise here and get the result using async/await.
-   }
+  let results = await delayedResultPromise(4, 5, 3000);
+  console.log(results);
+  // your code here, you should call delayedResultPromise here and get the result using async/await.
+}
 main(); // result will be shown in the console after <delayTime> seconds
--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,93 +1,100 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql2');
-const dotenv = require('dotenv').config();
+const mysql = require("mysql2");
+const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
 
-
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/static', express.static('public'));
+app.use("/static", express.static("public"));
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-  })  
-
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
 
 app.get("/home", (req, res) => {
-    const status = req.cookies.status;
-    res.render("home", {status});
-    });
-
-
+  const status = req.cookies.status;
+  res.render("home", { status });
+});
 
 app.post("/signup", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    connection.query(`SELECT * FROM user WHERE email = '${email}'`,(err, results) => {  
-        if (err) throw err;          
-        if (results.length > 0){
-            res.cookie("status", "exsisting");
-            res.redirect("/home")
-        } else {
-            connection.query(`INSERT INTO user (email, password) VALUES (?, ?)`, [email, password])
-            connection.query(`SELECT * FROM user WHERE email = '${email}'`, (err, results) => {
-            res.cookie("logEmail",results[0].email);
-            res.cookie("authorId",results[0].id);
+  connection.query(
+    `SELECT * FROM user WHERE email = '${email}'`,
+    (err, results) => {
+      if (err) throw err;
+      if (results.length > 0) {
+        res.cookie("status", "exsisting");
+        res.redirect("/home");
+      } else {
+        connection.query(`INSERT INTO user (email, password) VALUES (?, ?)`, [
+          email,
+          password,
+        ]);
+        connection.query(
+          `SELECT * FROM user WHERE email = '${email}'`,
+          (err, results) => {
+            res.cookie("logEmail", results[0].email);
+            res.cookie("authorId", results[0].id);
             res.redirect("/member");
-            })
-        }
-    })
+          }
+        );
+      }
+    }
+  );
 });
-
 
 app.post("/signin", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    
-connection.query(`SELECT * FROM user WHERE email = '${email}' AND password = '${password}'`, (err, results) => {
-        if (err) throw err;
-        if (results.length > 0) {
-            res.cookie("logEmail",results[0].email);
-            res.cookie("authorId",results[0].id);
-            res.redirect('/member');
-        } else {
-            res.cookie("status", "incorrect");
-            res.redirect("/home")
-        }
-      });
-});
+  const email = req.body.email;
+  const password = req.body.password;
 
+  connection.query(
+    `SELECT * FROM user WHERE email = '${email}' AND password = '${password}'`,
+    (err, results) => {
+      if (err) throw err;
+      if (results.length > 0) {
+        res.cookie("logEmail", results[0].email);
+        res.cookie("authorId", results[0].id);
+        res.redirect("/member");
+      } else {
+        res.cookie("status", "incorrect");
+        res.redirect("/home");
+      }
+    }
+  );
+});
 
 app.get("/member", (req, res) => {
-    const logEmail = req.cookies.logEmail;
-    res.clearCookie("status")
-    res.render("member", {logEmail})
+  const logEmail = req.cookies.logEmail;
+  res.clearCookie("status");
+  res.render("member", { logEmail });
 });
 
-
 app.post("/article", (req, res) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    const authorId = req.cookies.authorId;
-    
-    connection.query(`INSERT INTO article (title, content, author_id) VALUES (?, ?, ?)`, [title, content, authorId])  
-    res.redirect("/logout");
-    
-    })
+  const title = req.body.title;
+  const content = req.body.content;
+  const authorId = req.cookies.authorId;
 
-app.get("/logout", (req,res)=>{
-    res.clearCookie("logEmail")
-    res.clearCookie("authorId")
-    res.render("logout")
-})
+  connection.query(
+    `INSERT INTO article (title, content, author_id) VALUES (?, ?, ?)`,
+    [title, content, authorId]
+  );
+  res.redirect("/logout");
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("logEmail");
+  res.clearCookie("authorId");
+  res.render("logout");
+});
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
