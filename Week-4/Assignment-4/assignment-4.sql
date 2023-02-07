3ac2d5a4fb4c994d8eb218fd9276a1405faf4b60
5a6r1na <119278456+5a6r1na@users.noreply.github.com>
2023-02-07 20:24:59 +0800

    modified assignment-4.sql

--- Week-4/Assignment-4/assignment-4.sql
+++ Week-4/Assignment-4/assignment-4.sql
@@ -3,4 +3,4 @@
 SELECT article.title, article.content, user.email FROM article INNER JOIN user ON article.author_id = user.id;
 
 --Write another SQL statement to select articles from 7th to 12th sorted by id.
-SELECT article.id, article.title, article.content FROM article WHERE article.id BETWEEN 7 AND 12 ORDER BY article.id;
+SELECT article.id, article.title, article.content FROM article WHERE article.id ORDER BY article.id LIMIT 6,6;
